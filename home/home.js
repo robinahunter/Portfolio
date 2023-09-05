3a5e5da45ebd7e9c193ff11e2bb1ead9e06714e7
robin hunter <rbnrggs@yahoo.com>
2023-09-04 15:04:20 -1000

    update css js

---
+++ home/home.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
+
+document.querySelectorAll(".navbar").forEach(n => n.
+   addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navbar.classList.remove("active");
+   }))
