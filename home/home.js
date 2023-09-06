85a15c7dc0fbce1b52662c0db0fddcea19b2eb1f
robin hunter <rbnrggs@yahoo.com>
2023-09-05 17:08:18 -1000

    carousel

--- home/home.js
+++ home/home.js
@@ -1,6 +1,6 @@
 //JS for page transitions using swup
 
-const swup = newSwup()
+// const swup = newSwup()
 
 //JS for hamburger menu
 
@@ -17,3 +17,33 @@ document.querySelectorAll(".navbar").forEach(n => n.
         hamburger.classList.remove("active");
         navbar.classList.remove("active");
    }))
+
+   // CAROUSEL SECTION automatic slide function ---------------------------------------------------
+let currentImgIndex = 0;
+
+const images = document.getElementsByClassName('images');
+
+function automaticSlide () {
+    setTimeout(automaticSlide, 1000);
+let pics;
+const img  = document.querySelectorAll('.images');
+for (pics=0; pics<img.length; pics++) {
+    img[pics].style.display="none";
+}
+currentImgIndex++;
+if (currentImgIndex > img.length) {
+    currentImgIndex = 1;
+    }
+    img[currentImgIndex-1].style.display="block";
+}
+automaticSlide();
+
+const pause = document.querySelector('.pause')  
+
+pause.addEventListener('click', () => {
+    
+    if(currentImgIndex >= images.length) {
+        currentImgIndex = 0;
+        } 
+})
+       // CAROUSEL SECTION automatic slide function ---------------------------------------------------
