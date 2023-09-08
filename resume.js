bf3039a60494f0881cbc582fac8945c8728acf74
robin hunter <rbnrggs@yahoo.com>
2023-09-08 13:24:31 -1000

    update resume page

--- resume.js
+++ resume.js
@@ -17,3 +17,27 @@ const closeModal = () => {
     }
 
 close.addEventListener('click', closeModal)
+
+   // CAROUSEL SECTION automatic slide function 
+   let currentImgIndex = 0;
+
+   const imageBtl = document.getElementsByClassName('imageBtl');
+   
+   function automaticSlide () {
+       setTimeout(automaticSlide, 500);
+   
+   const img  = document.querySelectorAll('.imageBtl');
+   for (pics=0; pics<img.length; pics++) {
+       img[pics].style.display="none";
+   }
+   currentImgIndex++;
+   if (currentImgIndex > img.length) {
+       currentImgIndex = 1;
+       }
+     img[currentImgIndex-1].style.display="block";
+   }
+   automaticSlide();
+      
+   if(currentImgIndex >= images.length) {
+       currentImgIndex = 0;
+   } 
